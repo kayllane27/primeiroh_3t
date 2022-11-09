7e548868466c5579e73ae394c6a06e4a83761a97
kayllane27 <110061144+kayllane27@users.noreply.github.com>
2022-11-09 18:14:02 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -70,23 +70,3 @@ function total() {
     document.getElementById("resultado").innerHTML = "Total: " + moeda(resultado);
     //document.write( "O Resultado é " + moeda(resultado) );
 }
-
-    function  calculeRaiz(){
-        let a = document.getElementById("a").value;
-        let b = document.getElementById("b").value;
-        let c = document.getElementById("c").value;
-        let delta = (b*b) - 4*a*c
-
-        if(delta < 0){
-        
-        }
-        else if (delta == 0){
-
-        } else {
-
-        }
-
-     }
-
-
-
